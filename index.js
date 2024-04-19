ecb7d185558f8d008e929bcf3232324ade2a6c63
CountryUruguay <152349929+CountryUruguay@users.noreply.github.com>
2024-04-18 23:26:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const client = new Client({ intents: 3276799 });
 client.on("ready", () => {
     console.log(`Encendido como: ${client.user.tag}`)
 });
-client.login("MTE5NjEzNjUzMjg0MzY5MjA4Mg.GG3Q3S.ZG75v7sv8TtXD0lKA2-O-H0nn8T0dBGspVfnA")
+client.login("TOKEN")
